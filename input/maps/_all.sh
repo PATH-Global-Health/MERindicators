188bb8012284cd6793287a217353f9aaf73d3231
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2024-05-03 16:19:25 -0400

    add beginning of fml tutorial

---
+++ input/maps/_all.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# ephemeral containers for testing (in-mem, no db)
+# docker run -p 8080:8080 -e hapi.fhir.default_encoding=xml hapiproject/hapi:latest
---
+++ input/maps/_convert.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+export HEADER1="content-type:application/fhir+json"
+export HEADER2="content-type:text/fhir-mapping"
+export ACCEPT="accept:application/fhir+json"
+
+export FHIR="https://test.ahdis.ch/matchbox/fhir"
+export CONVERT="${FHIR}/StructureMap/$convert"
+export MAP="${FHIR}/StructureMap"
+
+# curl --request POST --H 'accept: application/fhir+json' --H 'content-type: text/fhir-mapping' --data-binary @HIVObservation-HIVCondition.map --url 'https://test.ahdis.ch/matchbox/fhir/StructureMap/$convert' 
+
+function convertput {
+    curl -s -X POST -H ${ACCEPT} -H ${HEADER2} --data-binary @${1}.map --url ${CONVERT} | \
+    curl -s -X PUT -H ${HEADER1} --data-binary @- --url "${MAP}/${1}" | \
+    curl -s -X GET -H ${ACCEPT} --url "${MAP}/${1}" | \
+    jq .
+}
+
+convertput HIVObservation-HIVCondition
+
